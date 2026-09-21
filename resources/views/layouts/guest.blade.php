94d77cb3ddf915b1c73259eeaa597292e0e7ccc3
flingeflung <ergetr2@gmx.de>
2026-09-21 14:11:33 +0200

    Login: Logo auf breitem blauem Balken

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,8 +17,8 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div class="w-full sm:max-w-md px-6 py-6 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                <a href="/" class="flex justify-center mb-6">
-                    <img src="{{ asset('images/vectory-logo.svg') }}" alt="{{ config('app.name') }}" class="h-24 w-auto rounded-lg">
+                <a href="/" class="flex justify-center mb-6 py-8 bg-logo rounded-lg">
+                    <img src="{{ asset('images/vectory-logo.svg') }}" alt="{{ config('app.name') }}" class="h-20 w-auto">
                 </a>
 
                 {{ $slot }}
